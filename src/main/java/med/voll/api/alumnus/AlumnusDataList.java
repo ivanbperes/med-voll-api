3294d3e43ecc89bee5d635a1a4550da8207b4e26
Ecenture <ivanbperes81@gmail.com>
2023-11-20 12:14:18 -0300

    add adivisor to alumnus list

--- src/main/java/med/voll/api/alumnus/AlumnusDataList.java
+++ src/main/java/med/voll/api/alumnus/AlumnusDataList.java
@@ -3,9 +3,10 @@ package med.voll.api.alumnus;
 public record AlumnusDataList(
         String name,
         String course,
-        String classEntry) {
+        String classEntry,
+        String advisor) {
 
         public AlumnusDataList(Alumnus alumnus) {
-                this(alumnus.getName(), alumnus.getCourse(), alumnus.getClassEntry());
+                this(alumnus.getName(), alumnus.getCourse(), alumnus.getClassEntry(), alumnus.getGraduation().advisor);
         }
 }
