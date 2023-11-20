f62460f03bc20d1168e3ed7961d97b777c476c88
Ecenture <ivanbperes81@gmail.com>
2023-11-20 13:04:46 -0300

    pageable default size 10 and sort in doctor and alumnus list

--- src/main/java/med/voll/api/controller/AlumnusController.java
+++ src/main/java/med/voll/api/controller/AlumnusController.java
@@ -8,6 +8,7 @@ import med.voll.api.alumnus.AlumnusRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -27,7 +28,7 @@ public class AlumnusController {
     }
 
     @GetMapping
-    public Page<AlumnusDataList> list(Pageable pagination) {
+    public Page<AlumnusDataList> list(@PageableDefault(size = 10, sort = {"classEntry"}) Pageable pagination) {
         return repository.findAll(pagination).map(AlumnusDataList::new);
     }
 }
