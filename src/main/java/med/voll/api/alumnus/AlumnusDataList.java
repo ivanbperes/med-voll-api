be08f4da94fd35a8a9a883dd4ca1ba01c9bf6ff5
Ecenture <ivanbperes81@gmail.com>
2023-11-20 23:41:18 -0300

    add id to doctor and alumnus list

--- src/main/java/med/voll/api/alumnus/AlumnusDataList.java
+++ src/main/java/med/voll/api/alumnus/AlumnusDataList.java
@@ -1,12 +1,13 @@
 package med.voll.api.alumnus;
 
 public record AlumnusDataList(
+        Long id,
         String name,
         String course,
         String classEntry,
         String advisor) {
 
         public AlumnusDataList(Alumnus alumnus) {
-                this(alumnus.getName(), alumnus.getCourse(), alumnus.getClassEntry(), alumnus.getGraduation().advisor);
+                this(alumnus.getId(), alumnus.getName(), alumnus.getCourse(), alumnus.getClassEntry(), alumnus.getGraduation().advisor);
         }
 }
--- src/main/java/med/voll/api/doctor/DoctorListData.java
+++ src/main/java/med/voll/api/doctor/DoctorListData.java
@@ -1,12 +1,13 @@
 package med.voll.api.doctor;
 
 public record DoctorListData(
+        Long id,
         String nome,
         String email,
         String crm,
         Specialization especialidade) {
 
     public DoctorListData(Doctor doctor) {
-        this(doctor.getNome(), doctor.getEmail(), doctor.getCrm(), doctor.getEspecialidade());
+        this(doctor.getId(), doctor.getNome(), doctor.getEmail(), doctor.getCrm(), doctor.getEspecialidade());
     }
 }
